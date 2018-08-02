6f04ad51eabe0a62a57779fd953ab96c7decd2b8
himanshuk924 <himanshuk924@gmail.com>
2018-08-02 22:35:18 +0530

    Add files via upload

---
+++ OrganisationJar/pack1/Orgtest.java
@@ -0,0 +1,7 @@
+package pack1;
+import pack2.Org;
+public class Orgtest {
+	public static void main(String[] args) {
+		Org.orgfun();
+	}
+}
---
+++ OrganisationJar/pack2/Org.java
@@ -0,0 +1,7 @@
+package pack2;
+
+public class Org {
+	public static void orgfun() {
+		System.out.println("This is coming from Org");
+	}
+}
